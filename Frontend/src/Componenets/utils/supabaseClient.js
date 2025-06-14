005466a14bde4b033d599ed6028a85865d7e57ee
Prathviraj Holla <104517214+PRATHVIRAJ67@users.noreply.github.com>
2025-06-14 19:54:26 +0530

    Update supabaseClient.js

--- Frontend/src/Componenets/utils/supabaseClient.js
+++ Frontend/src/Componenets/utils/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/utils/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://nozzpehnfbukmptsnhoo.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5venpwZWhuZmJ1a21wdHNuaG9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNjAzMzMsImV4cCI6MjA1OTkzNjMzM30.RaSvnadBrgrbWssM-HDpwiQZy8DUk7UJZPR3cBPLbdE";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
